16c2cba9ad44fe59593125a765a2fccef38410d3
mjgumbs <mjgumbs.200@gmail.com>
2017-02-03 23:49:37 -0400

    dutch carribean

--- lib/phone/bq/bo.ex
+++ lib/phone/bq/bo.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BQ.BO do
   use Helper.Area
-  field :regex, ~r/(599)(7)(.{7})/
+  field :regex, ~r/(599)(7)(.{6})/
   field :area_name, "Bonaire"
   field :area_type, "state"
   field :area_abbreviation, "BO"
--- lib/phone/bq/sa.ex
+++ lib/phone/bq/sa.ex
@@ -1,6 +1,6 @@
 defmodule Phone.BQ.SA do
   use Helper.Area
-  field :regex, ~r/(599)(4)(.{7})/
+  field :regex, ~r/(599)(4)(.{6})/
   field :area_name, "Saba"
   field :area_type, "state"
   field :area_abbreviation, "SA"
